8debe42de711c30ef0844267e84b46dd449eadc0
Krituraj Anand <66024105+kjais1720@users.noreply.github.com>
2026-05-17 15:41:04 +0530

    Add: binary-tree-right-side-view - submission-3

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-3.go
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func rightSideView(root *TreeNode) []int {
+    mp := make(map[int]int)
+
+	var dfs func(root *TreeNode, depth int)
+	dfs = func(root *TreeNode, depth int) {
+		if root == nil {
+			return
+		}
+		if _, ok := mp[depth]; !ok {
+			mp[depth] = root.Val
+		}
+		dfs(root.Right, depth+1)
+		dfs(root.Left, depth+1)
+	}
+	// curr := &TreeNode{
+	// 	Right: root,
+	// }
+	dfs(root, 0)
+	res := make([]int, 0, len(mp))
+	for i := 0; i<len(mp); i++ {
+		res = append(res, mp[i])
+	}
+	return res
+}
